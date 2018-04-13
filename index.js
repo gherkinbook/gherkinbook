fa7af79389f92c903895259eb3b43c9442172977
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2018-04-13 17:28:27 +0100

    #9 Initial attempt to merge test results with GherkinBook feature store

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const glob = require("glob")
+const glob = require('glob')
 const crypto = require('crypto')
 const Gherkin = require('gherkin')
 const configFileName = '.gherkinbook'
@@ -12,36 +12,99 @@ if (!fs.existsSync(configFileName)) {
 
 const config = JSON.parse(fs.readFileSync(configFileName, 'utf8'))
 
-function findFeatureFiles (base, files, result) {
-  files = files || fs.readdirSync(base)
-  result = result || []
-  files.forEach(function (file) {
-    var newbase = path.join(base, file)
-    if (fs.statSync(newbase).isDirectory()) {
-      result = findFeatureFiles(newbase, fs.readdirSync(newbase), result)
-    } else {
-      if (file.endsWith('.feature')) {
-        result.push(newbase)
+function readTestResults () {
+  return new Promise(function (resolve, reject) {
+    glob(config.cucumberJson, function (err, files) {
+      console.log('Reading Test Results....')
+      if (err) {
+        console.error('Reading Test Results....Failed')
+        reject(err)
       }
-    }
+      var testResults = []
+      files.forEach(function (file) {
+        var content = fs.readFileSync(file, 'utf-8')
+        var results = JSON.parse(content) || []
+        testResults = testResults.concat(results)
+      })
+      console.log('Reading Test Results....Done')
+      resolve(testResults)
+    })
+  })
+}
+
+function readFeatures () {
+  return new Promise(function (resolve, reject) {
+    glob(config.source, function (err, files) {
+      console.log('Reading Feature Files...')
+      if (err) {
+        console.error('Reading Feature Files...Failed')
+        reject(err)
+      }
+      var parser = new Gherkin.Parser(new Gherkin.AstBuilder())
+      var features = []
+      files.forEach(function (fileName) {
+        var fileContent = fs.readFileSync(fileName, 'utf-8')
+        var feature = parser.parse(fileContent).feature || {}
+        feature.id = crypto.createHash('md5').update(fileName).digest('hex')
+        feature.uri = fileName
+        features.push(feature)
+      })
+      console.log('Reading Feature Files...Done')
+      resolve(features)
+    })
+  })
+}
+
+function findTestResult(tests, featureName, scenarioName, stepName) {
+  var result = {}
+  tests.forEach(feature => {
+    if (feature.name !== featureName) return
+    feature.elements.forEach(scenario => {
+      if (scenario.name !== scenarioName) return
+       scenario.steps.forEach(step => {
+         if (step.name !== stepName) return
+         result = step.result || {}
+       })
+    })
   })
   return result
 }
 
-glob(config.cucumberJson, function (er, files) {
-  console.log(files)
-})
+function merge () {
+  return new Promise(function (resolve, reject) {
+    Promise.all([readFeatures(), readTestResults()]).then(results => {
+      console.log('Merging Results...')
+      var features = results[0]
+      var tests = results[1]
 
-var files = findFeatureFiles(config.source)
-var parser = new Gherkin.Parser(new Gherkin.AstBuilder())
-var bundle = []
-files.forEach(function (fileName) {
-  var fileContent = fs.readFileSync(fileName, 'utf-8')
-  var feature = parser.parse(fileContent).feature || {}
-  feature.id = crypto.createHash('md5').update(fileName).digest('hex')
-  feature.uri = fileName
+      features.forEach(feature => {
+        feature.children.forEach(child => {
+          child.steps.forEach(step => {
+            step.result = findTestResult(tests, feature.name, child.name, step.text)
+          })
+        })
+      })
 
-  bundle.push(feature)
-})
-var gherkinbook = 'var gherkinbook =' + JSON.stringify(bundle)
-fs.writeFileSync(path.join(config.output, outputFileName), gherkinbook, 'utf-8')
+      console.log(JSON.stringify(features))
+      console.log('Merging Results...Done')
+      resolve({})
+    }).catch((err) => reject(err))
+  })
+}
+
+function bundle () {
+  var gherkinbookData = 'var gherkinbook =' + JSON.stringify(bundle)
+  var gherkinbookFile = path.join(config.output, outputFileName)
+  fs.writeFileSync(gherkinbookFile, gherkinbookData, 'utf-8')
+  console.log('GherkinBook data has been successfully generated')
+}
+
+function publish () {
+  merge().then(result => bundle(result)).catch(err => console.error(err))
+}
+
+function main () {
+  publish()
+}
+
+main()
